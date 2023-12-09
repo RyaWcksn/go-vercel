537350dbd31b85296ee86d7b02a87f070a176948
RyaWcksn <pram.aryawcksn@gmail.com>
2023-12-10 02:41:13 +0700

    Testing

--- api/index.go
+++ api/index.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"fmt"
 	"net/http"
-	"serverless/internal/domain"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -13,7 +12,6 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/ ", Handler)
-	server.HandleFunc("/anothers", domain.AHandler)
 
 
 	http.ListenAndServe(":8080", server)
